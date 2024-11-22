60f26d77bb78c515294c0a497e36daad61e227f9
starchcode <mrdave67@gmail.com>
2024-11-22 00:03:25 +0000

    make inactive window a style, adds commented out code to now show line numbers for inactive win

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,40 @@
 vim.opt.winbar = "%=%m %f"
 vim.opt.wrap = true
 vim.g.autoformat = false
+
+
+-- following lines will show line numbers for active window only (where the cursor is!)
+--
+-- vim.api.nvim_create_autocmd("WinEnter", {
+--   callback = function()
+--     vim.wo.number = true       -- Show line numbers
+--     vim.wo.relativenumber = true -- Optional: Show relative numbers
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("WinLeave", {
+--   callback = function()
+--     vim.wo.number = false      -- Hide line numbers
+--     vim.wo.relativenumber = false -- Hide relative numbers
+--   end,
+-- })
+
+
+-- Define highlight groups for active and inactive windows
+vim.cmd([[
+  highlight ActiveWindow ctermbg=NONE guibg=NONE
+  highlight InactiveWindow ctermbg=darkgray guibg=#1f2330
+]])
+
+-- Apply the highlight groups based on window activity
+vim.api.nvim_create_autocmd("WinEnter", {
+  callback = function()
+    vim.wo.winhighlight = "Normal:ActiveWindow"
+  end,
+})
+
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    vim.wo.winhighlight = "Normal:InactiveWindow"
+  end,
+})
