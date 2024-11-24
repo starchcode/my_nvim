a75442ed16086da4c9e48c65c3cf6fccdda04cea
starchcode <mrdave67@gmail.com>
2024-11-24 17:56:20 +0000

    typscript works on js files only

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,20 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    -- make sure mason installs the server
+    servers = {
+      vtsls = {
+        -- explicitly add default filetypes, so that we can extend
+        -- them in related extras
+        filetypes = {
+          -- "javascript",
+          -- "javascriptreact",
+          -- "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+      },
+    },
+  },
+}
