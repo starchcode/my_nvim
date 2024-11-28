e02f1143efadeaad83d3afc4a2cfaf9ffd0cba6f
starchcode <mrdave67@gmail.com>
2024-11-28 14:45:53 +0000

    diff view plugin with keymas added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>dv", function()
+  -- Get the current branch name
+  local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD"):gsub("%s+", "")
+
+  local main_branch_name = "master" -- change this to the name of the branch you want to compare to.
+
+  -- Open Diffview comparing master and the current branch
+  vim.cmd("DiffviewOpen " .. main_branch_name .. "..." .. branch)
+end, { desc = "Diffview: Compare current branch with master" })
---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,27 @@
+return {
+  -- Other plugins...
+
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" }, -- Ensure plenary.nvim is included
+    config = function()
+      require("diffview").setup({
+        -- Customize your Diffview configuration here
+        enhanced_diff_hl = true, -- Example option for enhanced highlighting
+        keymaps = {
+          view = {
+            -- Custom keybindings inside the Diffview view
+          },
+          file_panel = {
+            -- Custom keybindings for file panel
+          },
+        },
+      })
+    end,
+  },
+}
+
+
+ -- :DiffviewOpen master...<CURRENT BRANCH NAME> 
+-- comparest master with current branch. 
+
