562af64c020614a03c3a7b70ada66c235b643551
starchcode <mrdave67@gmail.com>
2025-07-15 16:23:12 +0100

    can read axlsx files like a ruby file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+
+-- reads axlsx files like a ruby file
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.axlsx",
+  callback = function()
+    vim.bo.filetype = "ruby"
+  end,
+})
